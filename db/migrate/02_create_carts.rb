d4a7027cc8d804217fed4fa78bc425d4223f7fd3
ccchippp <clempke90@gmail.com>
2021-01-27 18:37:03 -0500

    before upload to submain

--- db/migrate/02_create_carts.rb
+++ db/migrate/02_create_carts.rb
@@ -2,6 +2,14 @@ class CreateCarts < ActiveRecord::Migration[6.1]
     def change
       create_table :carts do |t|
         t.references :user
+        serialize :config, Hash
       end
     end
   end
+
+# chip.cart.to_a.map(&:serializable_hash)
+
+# hash = { }
+# hash[:a] = 'a'
+# hash[:a]
+# # => 'a'
