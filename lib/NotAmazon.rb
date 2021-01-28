4c517aa48496ecb94a444575977485a6cd9dd859
Abraham Aviles <aaaviles1995@gmail.com>
2021-01-28 17:12:06 -0500

    5:11

--- lib/NotAmazon.rb
+++ lib/NotAmazon.rb
@@ -1,9 +1,22 @@
 class NotAmazon
     
     @@username = ""
+    @@user = nil
+
+    def self.user
+        @@user
+    end
+
+    def self.user=(user)
+        @@user = user
+    end
 
     def self.username
         @@username
+    end
+
+    def self.username=(username)
+        @@username = username
     end
 
     def self.reponse
@@ -20,11 +33,12 @@ class NotAmazon
         puts "Hello welcome to NotAmazon™"
         puts "Please enter your Username: "
         NotAmazon.username = gets.chomp
+        NotAmazon.user = User.create(username: NotAmazon.username)
         puts "Hello #{NotAmazon.username}"
         puts "How can we help you? Want to buy something? (yes or no)"
         answer = NotAmazon.reponse
         if answer == "yes"
-            NotAmazon.purchase
+            NotAmazon.make_purchase(NotAmazon.user)
             puts "Thank you for your visit!"
         elsif answer == "no"
             puts "WHY YOU WASTING MY TIME THEN! LEAVE I HAVE OTHER CUSTOMERS... "
@@ -32,8 +46,8 @@ class NotAmazon
         puts "Make sure to leave 5 stars on yelp."
     end
 
-    def self.purchase
-        puts "test"
+    def self.make_purchase(user)
+        puts "What would you like to purchase from our selection: "
     end
 
 end
