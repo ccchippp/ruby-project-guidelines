554e7ca2636978d6a9f28cd480577231d90c63b1
Abraham Aviles <aaaviles1995@gmail.com>
2021-01-27 22:56:36 -0500

    10:56

--- db/migrate/03_create_items.rb
+++ db/migrate/03_create_items.rb
@@ -1,7 +1,6 @@
 class CreateItems < ActiveRecord::Migration[6.1]
     def change
       create_table :items do |t|
-        t.integer :id
         t.string :item
         t.float :price 
         t.integer :quantity
--- db/schema.rb
+++ db/schema.rb
@@ -22,10 +22,13 @@ ActiveRecord::Schema.define(version: 4) do
   create_table "items", force: :cascade do |t|
     t.string "item"
     t.float "price"
+    t.integer "quantity"
   end
 
   create_table "orders", force: :cascade do |t|
     t.boolean "processed"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
