59f81777a40499b86d799f6d8afac75e8fc59bfb
ccchippp <clempke90@gmail.com>
2021-01-29 09:51:54 -0500

    more updates

--- bin/run.rb
+++ bin/run.rb
@@ -24,20 +24,6 @@ chips_cart.items << socks
 
 binding.pry
 
-# chips_cart = Cart.create
-
-# coffee = Item.create(item: "coffee", price: 16.00)
-# shirt = Item.create(item: "shirt", price: 20.00)
-# socks = Item.create(item: "socks", price: 5.00)
-
-# chip.carts << chips_cart
-
-# temp = Cart.find_by(user_id: chip.id)
-# p temp.items == []
-
-# chips_cart.items << coffee
-# chips_cart.items << shirt
-# chips_cart.items << socks
 
 # p temp.items != []
 # p abe.carts
@@ -53,7 +39,6 @@ things to do:
         adding stuff to cart
         probably write seperate method for checking out
     > in owner_task:
-        customer list
         check orders
         add new item to inventory
 =end
