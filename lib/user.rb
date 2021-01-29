e84402459d10a9063fc4deb1f63a2b22a011e409
ccchippp <clempke90@gmail.com>
2021-01-29 09:20:43 -0500

    need more time

--- lib/user.rb
+++ lib/user.rb
@@ -4,10 +4,21 @@ class User < ActiveRecord::Base
 
     # Users can view their cart's contents
     def my_cart
-        self.carts.collect {|i| i.items}
+        self.carts.collect { |i| i.items }
     end
     # Users can add products to their cart
     def add_item(product)
         self.carts.first << product
     end
+
+    # def remove_item(product)
+    #     trash = self.carts.select { |i| i.items == product }
+    #     if trash != nil 
+    #         self.carts.select { |i| i.items == trash }.delete
+    #         end
+    # end
+
+
+
+
 end
