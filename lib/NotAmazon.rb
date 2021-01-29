b7820ca82dcea7edd9d27cee98c36224e8bccbcb
Abraham Aviles <aaaviles1995@gmail.com>
2021-01-29 12:16:04 -0500

    owner task done?

--- lib/NotAmazon.rb
+++ lib/NotAmazon.rb
@@ -1,7 +1,15 @@
 class NotAmazon < ActiveRecord::Base
     
     @@username = ""
-    @@backroom = {"coffee" => 16.00, "shirt" => 20.00, "socks" => 5.00}
+
+    # defualt items store always tends to have
+    @@backroom = {
+        "coffee" => 16.00, "shirt" => 20.00, "socks" => 5.00
+        , "coffee" => 16.00, "shirt" => 20.00, "socks" => 5.00
+        , "coffee" => 16.00, "shirt" => 20.00, "socks" => 5.00
+        , "coffee" => 16.00, "shirt" => 20.00, "socks" => 5.00
+        , "coffee" => 16.00, "shirt" => 20.00, "socks" => 5.00
+    }
 
     def self.backroom
         @@backroom
@@ -16,24 +24,24 @@ class NotAmazon < ActiveRecord::Base
     end
 
     #=> helper method checking for response to be yes or no
-    def self.reponse
-        temp = gets.chomp
+    def self.response
+    temp = gets.chomp
         if temp == "yes" || temp == "no"
             return temp
          else
              puts "Please enter yes or no: "
-             NotAmazon.reponse
+             NotAmazon.response
         end
     end
 
     #=> helper method checks if user exists in database
-    def self.user_in_database
-        if !(User.find_by(username: NotAmazon.username))
-            return false
-        else
-            return true
-        end
-    end
+    # def self.user_in_database
+    #     if !(User.find_by(username: NotAmazon.username))
+    #         return false
+    #     else
+    #         return true
+    #     end
+    # end
 
     #=> Main Store Method
     def self.open_store
@@ -53,7 +61,7 @@ class NotAmazon < ActiveRecord::Base
             puts "      <<5. Add New Item to Inventory>>"
             puts "  <<Exit>>"
             NotAmazon.owner_task
-        elsif NotAmazon.user_in_database
+        elsif User.find_by(username: NotAmazon.username)
             user = User.find_by(username: NotAmazon.username)
             puts "Hello #{NotAmazon.username}, Welcome back to NotAmazon, happy to see you again."
         else
@@ -64,9 +72,10 @@ class NotAmazon < ActiveRecord::Base
             puts "Hello #{NotAmazon.username}, Welcome to NotAmazon!"
         end
 
+        # If user is the owner, ask if they wanna buy something
         if NotAmazon.username != "Owner" && NotAmazon.username != "owner"
             puts "How can we help you? Want to buy something? (yes or no)"
-            answer = NotAmazon.reponse
+            answer = NotAmazon.response
             if answer == "yes"
                 NotAmazon.make_purchase(user)
                 puts "Thank you for your visit!"
@@ -79,10 +88,13 @@ class NotAmazon < ActiveRecord::Base
         puts "Have A good day. Good Bye! ヽ(^‥^=ゞ)"
     end
 
+    # purchase method for user, adds items to cart and lets user checkout or just leave
     def self.make_purchase(user)
+
         cart = Cart.find_by(user_id: user.id)
         puts "Would you like to add to your cart? (yes or no)"
-        answer = NotAmazon.reponse
+        answer = NotAmazon.response
+
         if answer == "yes"
             puts "What would you like to add? This is our current selection of what's in stock today: "
             if Item.all.empty?
@@ -93,28 +105,37 @@ class NotAmazon < ActiveRecord::Base
                 # some code here
             end
         end
+
         if cart.items.empty?
             puts "Your cart is empty."
         end
+
     end
 
     # Task input for owner
     def self.owner_task
-        reponse = gets.chomp
-        if reponse == "1" #=> check Stock
+
+        response = gets.chomp
+
+        # owner tasks
+        if response == "1" #=> check Stock
             Item.display_all_items
-        elsif reponse == "2" #=> restock inventory
+        elsif response == "2" #=> restock defualt items inventory (item database)
             NotAmazon.backroom.each{|item_name, item_price| Item.create(item: item_name, price: item_price)}
-        elsif reponse == "3" #=> check customer list
+        elsif response == "3" #=> check customer list
             User.display_all_users
-        elsif reponse == "4" #=> check orders
+        elsif response == "4" #=> check orders
             Orders.display_all_orders
-        elsif reponse == "5" #=> add new item to inventory
-            #code
-            puts "you input 5"
-        elsif reponse == "exit"
-            puts "you input 'exit'"
-        elsif reponse == "help"
+        elsif response == "5" #=> add new item to inventory
+            puts "What's the name of the new item you're adding? (enter string)"
+            item_name = gets.chomp
+            puts "How much does #{item_name} cost? (enter integer or float)"
+            item_price = gets.chomp.to_f
+            new_item = Item.create(item: item_name, price: item_price)
+            puts "New item >>#{new_item}<< has been added to the Inventory."
+        elsif response == "exit" #=> exits
+            puts "Ok! Peace out Boss! (☞ ͡° ͜ʖ ͡°)☞"
+        elsif response == "help"
             puts "<<** These are the task you can work on today. Please enter a digit <1 to 5> or enter 'exit' to quit out **>>"
             puts "      <<1. Check Stock >>"
             puts "      <<2. Restock Inventory>>"
@@ -129,7 +150,18 @@ class NotAmazon < ActiveRecord::Base
             NotAmazon.owner_task
         end
 
-        puts "Would you like to do another task?"
+        # ask owner if they want to do more task if they didn't try to exit
+        if response != "exit"
+            puts "Would you like to do another task? (yes or no)"
+            response = NotAmazon.response
+            if response == "yes"
+                puts "What would you like to work on today?"
+                NotAmazon.owner_task
+            else
+                puts "Ok! Peace out Boss! (☞ ͡° ͜ʖ ͡°)☞"
+            end            
+        end
+
     end
 
 end
